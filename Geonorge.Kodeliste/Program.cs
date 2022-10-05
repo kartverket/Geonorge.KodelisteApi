6a345911ea2602dc126db941eedfd35e0d70dcdd
Dag Olav Dahle <dagolav@arkitektum.no>
2022-10-05 16:42:55 +0200

    Use routing

--- Geonorge.Kodeliste/Program.cs
+++ Geonorge.Kodeliste/Program.cs
@@ -27,5 +27,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
